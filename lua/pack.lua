ac055fc6f27b305ea6acafd2eaad5e25b1d0eb70
snowinter <1944129869@qq.com>
2025-03-29 17:02:09 +0800

    add_xpack

---
+++ lua/pack.lua
@@ -0,0 +1,21 @@
+includes("@builtin/xpack")
+
+xpack("test")
+    if is_plat("windows", "mingw") then
+        set_formats("zip")
+        add_installfiles(path.join(os.projectdir(), get_config("outputdir")) .. "/(**)|**.pdb|**.lib")
+    else
+        set_formats("targz")
+        add_installfiles(path.join(os.projectdir(), get_config("outputdir")) .. "/(**)|**.sym|**.a")
+    end
+xpack_end()
+
+xpack("test_debug_symbol")
+    if is_plat("windows", "mingw") then
+        set_formats("zip")
+        add_installfiles(path.join(os.projectdir(), get_config("outputdir")) .. "/(**.pdb)", path.join(os.projectdir(), get_config("outputdir")) .. "/(**.lib)")
+    else
+        set_formats("targz")
+        add_installfiles(path.join(os.projectdir(), get_config("outputdir")) .. "/(**.sym)", path.join(os.projectdir(), get_config("outputdir")) .. "/(**.a)")
+    end
+xpack_end()
