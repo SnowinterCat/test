2f96d43e548821ace5ffbc3f5f11ae26f80a3445
snowinter <1944129869@qq.com>
2025-03-29 15:36:56 +0800

    fix_autoluanch

--- lua/auto.lua
+++ lua/auto.lua
@@ -30,9 +30,6 @@ function autofunc.library_autodefine(target)
 end
 
 function autofunc.binary_autoluanch(target)
-    if is_config("luanch", false) then
-        return
-    end
     local proj_luancher = project.target("luancher")
     local luancher = path.join(target:targetdir(), target:basename() .. (is_plat("windows", "mingw") and ".exe" or ""))
     os.cp(proj_luancher:targetfile(), luancher)
