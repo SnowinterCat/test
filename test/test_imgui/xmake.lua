031a95bfc3ddb39d46600fdcc66dc9d18da213b8
snowinter <1944129869@qq.com>
2025-03-30 10:59:53 +0800

    add_test_imgui

---
+++ test/test_imgui/xmake.lua
@@ -0,0 +1,18 @@
+target("test_imgui")
+    set_kind("shared")
+    set_targetdir("$(testdir)")
+
+    add_deps("luancher")
+    add_packages("spdlog", "imgui", "vulkansdk", "libsdl3")
+
+    -- add_includedirs("include", {public = true})
+    -- add_headerfiles("include/(**)")
+    -- add_headerfiles("src/(**.hpp)", {install = false})
+    add_files("src/**.cpp")
+
+    after_build(function (target) 
+        import("lua.auto", {rootdir = os.projectdir()})
+        auto().target_autoclean(target)
+        auto().binary_autoluanch(target)
+    end)
+target_end()
