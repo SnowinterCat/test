44076b327ff529130b74430fca1be346a75f5097
SnowinterCat <SnowinterCat@github.com>
2025-04-02 23:42:36 +0800

    [remove]deprecated

--- src/core/include/test/core/environment.hpp
+++ src/core/include/test/core/environment.hpp
@@ -7,21 +7,6 @@
 TEST_BEGIN
 
 /**
- * @brief 将 unicode(wchar_t)编码 的 argv 转换成 utf8(char)编码 的 argv
- *
- * @param argc 参数个数
- * @param wargv unicode(wchar_t)编码 的参数字符串指针列表的指针
- * @param args 转换后的 utf8(char)编码 的参数内容
- * @param argvector 转换后的 utf8(char)编码 的参数字符串指针列表
- * @return char** 转换后的 utf8(char)编码 的参数字符串指针列表的指针
- * @remarks 若 args
- * 非空，则转换后的内容会接在原内容之后，但原内容不会被当作参数。不建议出于此目的使用该函数，此功能仅为了保证当
- * args 非空时也能正常工作
- */
-TEST_CORE_API char **convert_argc_argv(size_t argc, const wchar_t *const *wargv, std::string &args,
-                                       std::vector<char *> &argvector);
-
-/**
  * @brief
  *
  * @param str
--- src/core/src/environment.cpp
+++ src/core/src/environment.cpp
@@ -8,23 +8,6 @@
 
 TEST_BEGIN
 
-char **convert_argc_argv(size_t argc, const wchar_t *const *wargv, std::string &args,
-                         std::vector<char *> &argvector)
-{
-    std::wstring_convert<std::codecvt_utf8<wchar_t>, wchar_t> converter = {};
-    argvector.resize(argc);
-
-    std::vector<size_t> pos(argc + 1, args.size());
-    for (size_t i = 0; i < argc; ++i) {
-        args.append(converter.to_bytes(wargv[i])).append(1, '\0');
-        pos[i + 1] = args.size();
-    }
-    for (size_t i = 0; i < argc; ++i) {
-        argvector[i] = &args[pos[i]];
-    }
-    return argvector.data();
-}
-
 std::string string_to_argc_argv(const std::string &str, int &argc,
                                 std::vector<const char *> &argvector)
 {
