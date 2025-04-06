494569c1a6184e1771b48b81e16e51ac6daed331
SnowinterCat <SnowinterCat@github.com>
2025-04-06 09:16:18 +0800

    [update]3rdwrap

--- src/3rdwrap/include/test/3rdwrap.hpp
+++ src/3rdwrap/include/test/3rdwrap.hpp
@@ -1,5 +1,5 @@
 #pragma once
 #include <test/3rdwrap/3rdwrap_library.h>
 
-#include "test/3rdwrap/vulkan.hpp"
-#include "test/3rdwrap/libsdl3.hpp"
+#include <test/3rdwrap/vulkan.hpp>
+#include <test/3rdwrap/libsdl3.hpp>
--- src/3rdwrap/include/test/3rdwrap/libsdl3.hpp
+++ src/3rdwrap/include/test/3rdwrap/libsdl3.hpp
@@ -1,4 +1,4 @@
 #pragma once
 #include <test/3rdwrap/3rdwrap_library.h>
 
-#include "test/3rdwrap/libsdl3/deleter.hpp"
+#include <test/3rdwrap/libsdl3/deleter.hpp>
