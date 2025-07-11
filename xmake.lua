cae8a6b1f086be47dd30cd3e83fe66010b032e09
SnowinterCat <SnowinterCat@github.com>
2025-07-11 17:46:48 +0800

    [update]

--- xmake.lua
+++ xmake.lua
@@ -1,37 +1,36 @@
 -- 工程设置
 set_project("test")
-set_version("0.0.1", {build = "$(buildversion)"})
+set_version("0.0.1", {build = "$(buildversion)", soname = true})
+set_xmakever("3.0.0")
+
+set_configvar("LEGAL_COPYRIGHT", "Copyright (C) 2024 SnowinterCat")
+set_configvar("PROJECT_NAME", "Test")
 
 -- 全局设置
 option("stdc",   {showmenu = true, default = 23, values = {23, 17}})
-option("stdcxx", {showmenu = true, default = 23, values = {23, 20, 17}})
-function stdc_ver() 
-    return get_config("stdc") and "c" .. get_config("stdc") or nil
-end
-function stdcxx_ver() 
-    return get_config("stdcxx") and "c++" .. get_config("stdcxx") or nil
-end
+option("stdcxx", {showmenu = true, default = 23, values = {26, 23, 20}})
+function stdc()   return "c"   .. tostring(get_config("stdc"))   end
+function stdcxx() return "c++" .. tostring(get_config("stdcxx")) end
 
+set_languages(stdc(), stdcxx())
 set_warnings("allextra")
-set_languages(stdc_ver(), stdcxx_ver())
-set_exceptions("cxx")
 set_encodings("utf-8")
-set_policy("package.cmake_generator.ninja", true)
+set_exceptions("cxx")
 
 -- 添加编译选项
 add_rules("mode.release", "mode.debug", "mode.releasedbg", "mode.minsizerel")
 add_rules("plugin.compile_commands.autoupdate", {lsp = "clangd", outputdir = ".vscode"})
 
 -- 编译设置
-option("3rd_kind",     {showmenu = true, default = "shared", values = {"static", "shared"}})
-option("buildversion", {showmenu = true, default = "0", type = "string"})
+option("3rd_kind",     {showmenu = true, default = get_config("kind"), values = {"static", "shared"}})
 option("outputdir",    {showmenu = true, default = path.join(os.projectdir(), "bin"), type = "string"})
+option("buildversion", {showmenu = true, default = "0", type = "string"})
 option("luanch",       {showmenu = true, default = nil, type = "string"})   -- 调试时，设置此值为要调试的组件名即可
 
-includes("@builtin/check")
-check_macros("has_std_out_ptr",  "__cpp_lib_out_ptr",  {name = "has_std_out_ptr", languages = stdcxx_ver(), includes = "memory"})
-check_macros("has_std_expected", "__cpp_lib_expected", {name = "has_std_expected", languages = stdcxx_ver(), includes = "expected"})
-check_macros("has_std_runtime_format", "__cpp_lib_format >= 202311L", {name = "has_std_runtime_format", languages = stdcxx_ver(), includes = "format"})
+includes("lua/check")
+check_macros("has_std_out_ptr",  "__cpp_lib_out_ptr",  {languages = stdcxx(), includes = "memory"})
+check_macros("has_std_expected", "__cpp_lib_expected", {languages = stdcxx(), includes = "expected"})
+check_macros("has_std_runtime_format", "__cpp_lib_format >= 202311L", {languages = stdcxx(), includes = "format"})
 
 -- 隐藏设置、隐藏目标、打包命令
 includes("lua/hideoptions.lua")
@@ -39,13 +38,13 @@ includes("lua/hidetargets.lua")
 includes("lua/pack.lua")
 
 -- 第三方库依赖
--- some third-libraries use our own configurations
+-- some of the third-party use our own configurations
 add_repositories("myrepo 3rd", {rootdir = os.scriptdir()})
 -- header-only libraries
-if not get_config("has_std_out_ptr") then
+if not has_config("has_std_out_ptr") then
     add_requires("out_ptr")
 end
-if not get_config("has_std_expected") then
+if not has_config("has_std_expected") then
     add_requires("tl_expected")
 end
 add_requires("cxxopts", {version = "3.x.x"})
@@ -66,6 +65,7 @@ add_requireconfs("**.fmt", {override = true, version = "11.0.x", configs = {shar
 add_requireconfs("**.nlohmann_json", {override = true, version = "3.x.x"})
 add_requireconfs("**.stb", {override = true, version = "x.x.x"})
 
+-- subdirectories
 includes("src/*/xmake.lua")
 includes("exec/*/xmake.lua")
 includes("test/*/xmake.lua")
